0ac33d89d7dcd40e3a6c10e697fd17b5aa28b881
aaron <aaronyu15@hotmail.com>
2026-01-14 17:30:33 +0000

    Fix again

--- snn/data/__init__.py
+++ snn/data/__init__.py
@@ -1,6 +1,6 @@
 """Data loading and preprocessing"""
 
-from .optical_flow_dataset import OpticalFlowDataset, EventDataset
+from .optical_flow_dataset import OpticalFlowDataset
 from .data_utils import load_events, load_flow, augment_data
 
 __all__ = [
