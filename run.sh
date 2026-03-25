87cb1d84f3bc9e6840bb6010e828be756cca98b4
Your Name <aaronyu@cmail.carleton.ca>
2026-03-25 12:10:28 -0400

    Fix memory leak issue

--- run.sh
+++ run.sh
@@ -1,7 +1,59 @@
 #!/bin/bash
+set -e
 
-if [ $# -eq 1 ]; then
-    python train.py --config snn/configs/event_snn_lite.yaml --name $1
-elif [ $# -eq 2 ]; then
-    python train.py --config snn/configs/event_snn_lite.yaml --name $1 --resume $2
-fi
+CONFIG="snn/configs/event_snn_lite.yaml"
+
+# Defaults
+sed -i 's/num_bins: [0-9]\+/num_bins: 5/g' $CONFIG
+sed -i 's/bin_interval_us: [0-9]\+/bin_interval_us: 5000/g' $CONFIG
+sed -i 's/base_ch: [0-9]\+/base_ch: 8/g' $CONFIG
+
+# 08 - 5 bins
+NAME="08_2000u_5bin"
+sed -i 's/bin_interval_us: [0-9]\+/bin_interval_us: 2000/g' $CONFIG
+python train.py --config snn/configs/event_snn_lite.yaml --name "${NAME}_2"
+python train.py --config snn/configs/event_snn_lite.yaml --name "${NAME}_3"
+python train.py --config snn/configs/event_snn_lite.yaml --name "${NAME}_4"
+
+NAME="08_5000u_5bin"
+sed -i 's/bin_interval_us: [0-9]\+/bin_interval_us: 5000/g' $CONFIG
+python train.py --config snn/configs/event_snn_lite.yaml --name "${NAME}_2"
+python train.py --config snn/configs/event_snn_lite.yaml --name "${NAME}_3"
+python train.py --config snn/configs/event_snn_lite.yaml --name "${NAME}_4"
+
+NAME="08_8000u_5bin"
+sed -i 's/bin_interval_us: [0-9]\+/bin_interval_us: 8000/g' $CONFIG
+python train.py --config snn/configs/event_snn_lite.yaml --name "${NAME}_2"
+python train.py --config snn/configs/event_snn_lite.yaml --name "${NAME}_3"
+python train.py --config snn/configs/event_snn_lite.yaml --name "${NAME}_4"
+
+sed -i 's/num_bins: [0-9]\+/num_bins: 10/g' $CONFIG
+# 09 - 10 bins
+NAME="09_2000u_10bin"
+sed -i 's/bin_interval_us: [0-9]\+/bin_interval_us: 2000/g' $CONFIG
+python train.py --config snn/configs/event_snn_lite.yaml --name "${NAME}_2"
+python train.py --config snn/configs/event_snn_lite.yaml --name "${NAME}_3"
+python train.py --config snn/configs/event_snn_lite.yaml --name "${NAME}_4"
+
+NAME="09_5000u_10bin"
+sed -i 's/bin_interval_us: [0-9]\+/bin_interval_us: 5000/g' $CONFIG
+python train.py --config snn/configs/event_snn_lite.yaml --name "${NAME}_2"
+python train.py --config snn/configs/event_snn_lite.yaml --name "${NAME}_3"
+python train.py --config snn/configs/event_snn_lite.yaml --name "${NAME}_4"
+
+NAME="09_8000u_10bin"
+sed -i 's/bin_interval_us: [0-9]\+/bin_interval_us: 8000/g' $CONFIG
+python train.py --config snn/configs/event_snn_lite.yaml --name "${NAME}_2"
+python train.py --config snn/configs/event_snn_lite.yaml --name "${NAME}_3"
+python train.py --config snn/configs/event_snn_lite.yaml --name "${NAME}_4"
+
+# 06 - 16 channels
+
+sed -i 's/num_bins: [0-9]\+/num_bins: 5/g' $CONFIG
+sed -i 's/bin_interval_us: [0-9]\+/bin_interval_us: 5000/g' $CONFIG
+sed -i 's/base_ch: [0-9]\+/base_ch: 16/g' $CONFIG
+
+NAME="06_16_channels"
+sed -i 's/bin_interval_us: [0-9]\+/bin_interval_us: 8000/g' $CONFIG
+python train.py --config snn/configs/event_snn_lite.yaml --name "${NAME}_3"
+python train.py --config snn/configs/event_snn_lite.yaml --name "${NAME}_4"
